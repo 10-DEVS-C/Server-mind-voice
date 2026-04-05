a24905a3461679fa92c05425d4c9544775c6a35e
Miqueaz <161628310+Miqueaz@users.noreply.github.com>
2026-04-05 14:19:30 -0700

    fix tags

--- app/modules/tags/schemas.py
+++ app/modules/tags/schemas.py
@@ -2,5 +2,6 @@ from marshmallow import Schema, fields
 
 class TagSchema(Schema):
     _id = fields.String(dump_only=True)
+    userId = fields.String(allow_none=True, dump_only=True)
     name = fields.String(required=True)
     createdAt = fields.DateTime(dump_only=True)
